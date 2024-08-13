d15443fce96b326ddd014bb2717ca12b52fc895f
minho <pmmh9395@gmail.com>
2024-08-13 15:22:21 +0900

    WebSocke을 통해서 접속한 client의 정보 보기

--- src/server.js
+++ src/server.js
@@ -16,4 +16,10 @@ const handleListen = () => console.log('listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server}); // ws와 http서버를 3000포트에 둘 다 생성
 
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection)
+
 server.listen(3000,handleListen);
