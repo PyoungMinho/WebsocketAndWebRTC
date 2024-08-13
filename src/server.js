417f42eb59c8cd86b8261b63224e8b000b4eb631
minho <pmmh9395@gmail.com>
2024-08-13 14:52:04 +0900

    WebSocket 추가

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import http from 'http';
+import {WebSocket} from "ws";
+
 
 const app = express();
 
@@ -8,7 +11,9 @@ app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("*", (req, res) => res.redirect("/"));
 
-
-console.log("hello");
 const handleListen = () => console.log('listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server}); // ws와 http서버를 3000포트에 둘 다 생성
+
+server.listen(3000,handleListen);
