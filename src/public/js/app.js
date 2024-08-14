cce7388f244c08d4c261a072ef999bc71a8330ef
minho <pmmh9395@gmail.com>
2024-08-14 17:04:02 +0900

    닉네임 : 채팅 채팅 메시지( left or joined ) 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -22,8 +22,10 @@ function showRoom(){
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
-    const form = room.querySelector("form");
-    form.addEventListener("submit", handleMessageSubmit);
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
 }
 
 function backendDone(msg) {
@@ -32,13 +34,23 @@ function backendDone(msg) {
 
 function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector("input");
+    const input = room.querySelector("#msg input");
     const value = input.value;
     socket.emit("new_message", input.value,roomName, () => {
         addMessage(`You: ${value}`);
     });
     input.value = "";
 }
+
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    const value = input.value;
+    socket.emit("nickname", input.value);
+    input.value = "";
+}
+
+
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
@@ -51,12 +63,12 @@ form.addEventListener("submit", handleRoomSubmit);
 
 
 
-socket.on("welcome",() => {
-    addMessage("Someone joined!");
+socket.on("welcome",(user) => {
+    addMessage(`${user} arrived!`);
 });
 
-socket.on(("bye"), ()=>{
-   addMessage("Someone left");
+socket.on(("bye"), (left)=>{
+   addMessage(`${left} left`);
 });
 
 socket.on(("new_message"), addMessage);
